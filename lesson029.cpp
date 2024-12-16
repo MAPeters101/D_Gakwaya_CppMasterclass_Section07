d017543e9306b0ed174bdade9a4c7ad168fe09c0
Mark Peters <mapeters101@earthlink.net>
2024-12-16 18:26:34 -0500

    Work on lesson029

---
+++ lesson029.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+/*
+consteval int get_value() {
+	return 3;
+}
+
+int main() {
+	constexpr int value = get_value();
+	cout << "value : " << value << endl;
+	return 0;
+}
+*/
