af384cdd976be72e1f5ef93ce3fe82553da5efcb
Mark Peters <mapeters101@earthlink.net>
2024-12-16 18:34:01 -0500

    Work on lesson030.

---
+++ lesson030.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	cout << "Hello World!" << endl;
+	cout << "Number1" << endl;
+	cout << "Number2" << endl;
+	cout << "Number3" << endl;
+
+	return 0;
+}
+*/
+
+
+
