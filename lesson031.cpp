4d868283acd3441bf65a8ffa481c6cfd1a112905
Mark Peters <mapeters101@earthlink.net>
2024-12-16 18:40:38 -0500

    Work on lesson031.

---
+++ lesson031.cpp
@@ -0,0 +1,22 @@
+
+// This brings in the tream library
+#include <iostream>
+
+using namespace std;
+
+/*
+	This is a block comment that includes multiple
+	lines of text
+*/
+
+/*
+int main() {
+	//This is going to print "Hello World!" to the console
+	cout << "Hello World!" << endl;
+
+
+	return 0;
+	//Program ends here.
+}
+
+*/
