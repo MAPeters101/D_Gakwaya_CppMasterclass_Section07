a90f862375d3ac136ba9c3a36fdf821813adbffd
Mark Peters <mapeters101@earthlink.net>
2024-12-17 12:50:55 -0500

    Complete assignment01.

---
+++ Assignment01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+	cout << "Where do you live? ";
+	string country;
+	cin >> country;
+	cout << "I've heard great things about " << country << ". I'd like to go there sometime.";
+
+	return 0;
+}
+*/
