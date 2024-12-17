cb151048d8abc9f7704940c921d20a2858171de8
Mark Peters <mapeters101@earthlink.net>
2024-12-16 22:31:35 -0500

    Work on lesson 033 up to 11:00.

---
+++ lesson033.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int first_number{ 13 };
+	int second_number{ 7 };
+
+	cout << "First number : " << first_number << endl;
+	cout << "Second number : " << second_number << endl;
+
+	int sum = first_number + second_number;
+	cout << "Sum : " << sum << endl;
+
+	return 0;
+}
