25c68a621c1b98b6fe45531a0648bf8aef24d676
Mark Peters <mapeters101@earthlink.net>
2024-12-16 22:21:43 -0500

    Work on lesson032.

---
+++ lesson032.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;;
+
+/*
+int main() {
+	//Compile time error
+	//cout << "Hello World in C++10!" << endl
+	cout << "Hello World in C++10!" << endl;
+
+	//Run time error
+	//int value = 7 / 0;
+
+
+	return 0;
+}
+*/
+
