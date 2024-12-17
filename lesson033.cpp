02c04c79d2436387a763cd40c626b2ad98212ebc
Mark Peters <mapeters101@earthlink.net>
2024-12-16 22:37:46 -0500

    Complete work on lesson033.

--- lesson033.cpp
+++ lesson033.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+int addNumbers(int first_param, int second_param) {
+	int result = first_param + second_param;
+	return result;
+}
+
+/*
 int main() {
 	int first_number{ 13 };
 	int second_number{ 7 };
@@ -12,5 +18,15 @@ int main() {
 	int sum = first_number + second_number;
 	cout << "Sum : " << sum << endl;
 
+	sum = addNumbers(25, 7);
+	cout << "Sum : " << sum << endl;
+
+	sum = addNumbers(30, 54);
+	cout << "Sum : " << sum << endl;
+
+	cout << "Sum : " << addNumbers(3,42) << endl;
+
 	return 0;
 }
+
+*/
