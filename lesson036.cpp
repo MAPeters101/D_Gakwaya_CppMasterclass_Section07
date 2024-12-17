8423a80adb1a857ec36a2824b0fa84d86451aae1
Mark Peters <mapeters101@earthlink.net>
2024-12-17 12:33:56 -0500

    Work on lesson036.

---
+++ lesson036.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+int main() {
+	//cout << "Hello C++20 " << endl;
+
+//int age{ 21 };
+//cout << "Age : " << age << endl;
+
+//cerr << "Error message : Something is wrong" << endl;
+//clog << "Log message : Something happened " << endl;
+
+//int age1;
+//std::string name;
+
+//cout << "Please type your name and age : " << endl;
+//cin >> name >> age1;
+
+//cout << "Hello " << name << " you are " << age1 << " years old!" << endl;
+
+	std::string full_name;
+	int age3;
+	cout << "Please type your full name and age : " << endl;
+	std::getline(std::cin, full_name);
+	cin >> age3;
+	cout << "Hello " << full_name
+		<< " you are " << age3 << " years old!" << endl;
+
+
+
+
+
+	return 0;
+}
+*/
