d0f531fc6e924055f4e466e7446cef34e70ad778
Mark Peters <mapeters101@earthlink.net>
2024-12-17 12:19:31 -0500

    Work on lesson034 to 14:00.

--- lesson034.cpp
+++ lesson034.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main() {
-	cout << "Hello C++20 " << endl;
+	//cout << "Hello C++20 " << endl;
 
-	int age{ 21 };
-	cout << "Age : " << age << endl;
+	//int age{ 21 };
+	//cout << "Age : " << age << endl;
 
-	cerr << "Error message : Something is wrong" << endl;
-	clog << "Log message : Something happened " << endl;
+	//cerr << "Error message : Something is wrong" << endl;
+	//clog << "Log message : Something happened " << endl;
 
+	int age1;
+	std::string name;
+
+	cout << "Please type your name and age : " << endl;
+	cin >> name;
+	cin >> age1;
+
+	cout << "Hello " << name << " you are " << age1 << " years old!" << endl;
 
 
 	return 0;
