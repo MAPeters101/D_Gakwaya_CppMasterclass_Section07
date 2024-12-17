da51a4f420d89820a504aded9dc60e7c97269ae4
Mark Peters <mapeters101@earthlink.net>
2024-12-17 12:05:07 -0500

    Work on lesson034 to 10:00.

---
+++ lesson034.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	cout << "Hello C++20 " << endl;
+
+	int age{ 21 };
+	cout << "Age : " << age << endl;
+
+	cerr << "Error message : Something is wrong" << endl;
+	clog << "Log message : Something happened " << endl;
+
+
+
+	return 0;
+}
+
+
